e55125a1cd590f8433f3f9db42b35c1fb52c64c2
dswyzx <dswyzx@live.com>
2023-03-03 17:13:47 +0800

    change filed path

---
+++ LeetCodeConsoleCore/2023/38Count_and_Say.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeConsoleCore
+{
+    class program_38Count_and_Say
+    {
+        static void Main38(string[] args)
+        {
+            var x = CountAndSay(6);
+        }
+
+        static string CountAndSay(int n)
+        {
+            if (n == 1)
+            {
+                return "1";
+            }
+            string old = CountAndSay(n - 1);
+            StringBuilder res = new StringBuilder();
+
+            char sorce = old[0];
+            int len = 1;
+            for (int i = 1; i < old.Length; i++)
+            {
+                if (sorce == old[i])
+                {
+                    len++;
+                }
+                else
+                {
+                    res.Append(len.ToString() + sorce);
+                    sorce = old[i];
+                    len = 1;
+                }
+            }
+            res.Append(len.ToString() + sorce);
+            return res.ToString();
+        }
+    }
+
+
+
+
+}
---
+++ LeetCodeConsoleCore/2023/45Jump_Game_II.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCodeConsoleCore
+{
+    class program_45Jump_Game_II
+    {
+        static void Main45(string[] args)
+        {
+            int[] nums = new int[] { 7, 0, 9, 6, 9, 6, 1, 7, 9, 0, 1, 2, 9, 0, 3 };//1, 2, 3
+            var x = Jump(nums);
+        }
+
+        public static int Jump(int[] nums)
+        {
+            if (nums.Length == 1)
+            {
+                return 0;
+            }
+            if (nums[0] >= nums.Length - 1)
+            {
+                return 1;
+            }
+            int jumps = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] + i >= nums.Length)
+                {
+                    jumps++;
+                    break;
+                }
+                int max = nums[i]; int add = 0;
+                for (int j = 1; j <= nums[i]; j++)
+                {
+                    if (nums[i + j] + j >= max || i + j == nums.Length - 1)
+                    {
+                        add = j;
+                        max = nums[i + j] + j;
+                    }
+                }
+                i += add - 1;
+                jumps++;
+                if (i >= nums.Length - 2)
+                {
+                    break;
+                }
+            }
+            return jumps;
+        }
+    }
+
+
+}
